9ac891000d27cda5ba695701061a2bee9ed3f1df
heiz99 <haziqazfar041@gmail.com>
2020-06-04 20:59:19 +0800

    compared source code with built and original

--- components/NavButton.js
+++ components/NavButton.js
@@ -6,8 +6,7 @@ import "./NavButton.scss";
 const NavButton = props => (
   <Link href={props.path}>
     <div
-      className={`NavButton 
-      ${ props.router.pathname === props.path ? "active" : "" }`}
+      className={`NavButton ${ props.router.pathname === props.path ? "active" : "" }`}
     >
       <div className="Icon">{props.icon}</div>
       <span className="Label">{props.label}</span>
